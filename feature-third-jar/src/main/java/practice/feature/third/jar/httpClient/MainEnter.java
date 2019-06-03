2791b92861680f1b446cdeeb710a023af698844a
jack <duqiang.dq@alibaba-inc.com>
2019-06-03 23:10:19 +0800

    add httpClient to third jar module

---
+++ feature-third-jar/src/main/java/practice/feature/third/jar/httpClient/MainEnter.java
@@ -0,0 +1,11 @@
+package practice.feature.third.jar.httpClient;
+
+/**
+ * @author jack
+ */
+public class MainEnter {
+
+    public static void main(String[] args) {
+        System.out.println("hello HttpClient!");
+    }
+}
