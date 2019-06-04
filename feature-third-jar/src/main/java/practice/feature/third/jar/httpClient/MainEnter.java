aabb62b9fc7926d4d92ec2cc68d6e1a727bdfb25
jack <duqiang.dq@alibaba-inc.com>
2019-06-04 13:33:59 +0800

    import httpClient pom

--- feature-third-jar/src/main/java/practice/feature/third/jar/httpClient/MainEnter.java
+++ feature-third-jar/src/main/java/practice/feature/third/jar/httpClient/MainEnter.java
@@ -1,5 +1,8 @@
 package practice.feature.third.jar.httpClient;
 
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
 /**
  * @author jack
  */
@@ -7,5 +10,8 @@ public class MainEnter {
 
     public static void main(String[] args) {
         System.out.println("hello HttpClient!");
+
+        CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
+
     }
 }
