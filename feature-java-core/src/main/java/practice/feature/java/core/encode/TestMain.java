f177dcfc2ca1f602ab2f9425aee03205e8d8f055
duqiang.dq <duqiang.dq@autonavi.com>
2021-11-24 14:22:24 +0800

    aaaa

--- feature-java-core/src/main/java/practice/feature/java/core/encode/TestMain.java
+++ feature-java-core/src/main/java/practice/feature/java/core/encode/TestMain.java
@@ -10,19 +10,7 @@ public class TestMain {
 
     public static void main(String[] args) {
 
-        String encode1 = new String(
-            encoder.encode(
-                encoder.encode("LTAI4FwoZ8t4BQyN7vJQbo7K==eZHCy4vg8fcLtVJlhzHF2iDFvni4Wr".getBytes(StandardCharsets.UTF_8))
-            )
-        );
-        System.out.println(encode1);
-        System.out.println("VEZSQlNXVXlhbmhLT1VZd2JFeElUejA5U2tObWFUSkVhME0zZEVob2RGbDRNVmh5UVZaU1VFUkRXRmx0YTFacA==");
 
-        String accessIdReal = new String(decoder.decode(decoder
-            .decode("VEZSQlNXVXlhbmhLT1VZd2JFeElUejA5U2tObWFUSkVhME0zZEVob2RGbDRNVmh5UVZaU1VFUkRXRmx0YTFacA==")));
-        String[] accessKeyReal = accessIdReal.split("==");
-        System.out.println(accessKeyReal[0]);
-        System.out.println(accessKeyReal[1]);
     }
 }
 
